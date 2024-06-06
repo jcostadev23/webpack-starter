e21f7f0f2ffc2f9bc01bff0eebb35122384210c9
Jose Costa <carlos_jesus_costa@hotmail.com>
2024-06-06 22:51:56 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import message from "./messsage";
+import "./css/style.css";
+
+const teste = () => {
+    console.log(message);
+}
+
+teste()
---
+++ src/messsage.js
@@ -0,0 +1,6 @@
+const message = {
+  id: 1,
+  name: "costa",
+};
+
+export default message;
